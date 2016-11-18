87d83da431dde65d07b46e58822902e7e03cc7f8
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-17 17:54:57 -0800

    check in

--- timeplot.go
+++ timeplot.go
@@ -1,11 +1,15 @@
 package gomap
 
 import (
+	"net/http"
 	"html/template"
 )
+var t, err = template.ParseFiles("base.html")
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  t, _ = template.ParseFiles("templates/base.html", "templates/index.html")
-  t.ExecuteTemplate(w, "layout", "")
+	t.ExecuteTemplate(w, "layout", "")
 }
 
+func init() {
+  http.HandleFunc("/hello", hello)
+}
