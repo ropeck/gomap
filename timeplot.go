62e70d2c8a6ae11f44da2008500fd05363c7e2fa
Rodney Peck <ropeck@ropeck.mtv.corp.google.com>
2016-11-17 18:45:10 -0800

    checkin

--- timeplot.go
+++ timeplot.go
@@ -1,13 +1,16 @@
 package gomap
 
 import (
+	"directions"
 	"net/http"
 	"html/template"
 )
-var t, err = template.ParseFiles("base.html")
+var t, err = template.ParseFiles("base.html", "index.html")
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	t.ExecuteTemplate(w, "layout", "")
+	var m = make(map[string]string)
+	m["apikey"] = directions.Apikey(r)
+	t.ExecuteTemplate(w, "layout", m)
 }
 
 func init() {
