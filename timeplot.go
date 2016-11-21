ef1e1d5ad9acbe7aa3b25978c4d66fe03d26cf22
Rodney Peck <ropeck@google.com>
2016-11-21 14:35:15 -0800

    working on data urls

--- timeplot.go
+++ timeplot.go
@@ -5,7 +5,19 @@ import (
 	"net/http"
 	"encoding/json"
 	"html/template"
+	"time"
 )
+
+func drawday(td time.Time) ([][]string) {
+  return drawday_base(td, false, false)
+}
+
+func drawday_base(td time.Time, reverse bool, cache bool) ([][]string){
+  data := [][]string{{"time.Time", "Leave", "Expected", "Delay"}}
+  data = append(data, []string{"0","1","2"})
+  return data
+}
+
 
 func hello(w http.ResponseWriter, r *http.Request) {
      t, _ := template.ParseFiles("base.html", "index.html")
@@ -24,7 +36,8 @@ func arrive(w http.ResponseWriter, r *http.Request) {
 func arrivedata(w http.ResponseWriter, r *http.Request) {
      d := directions.NewDirections(r)
      d.Directions()
-     data := [][]string{{"Time", "Leave", "Expected", "Delay"}}
+     td := time.Now()
+     data := drawday(td)
      // for h in drawday(td):
      //   data.append( stuff from h )
      b, _ := json.Marshal(data)
