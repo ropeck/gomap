c35f9897fdc95931991efcae2e739d9f0d4401c7
Rodney Peck <ropeck@google.com>
2016-11-21 14:19:43 -0800

    checkin

--- timeplot.go
+++ timeplot.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/ropeck/directions"
 	"net/http"
+	"encoding/json"
 	"html/template"
 )
 
@@ -20,7 +21,18 @@ func arrive(w http.ResponseWriter, r *http.Request) {
      t.ExecuteTemplate(w, "layout", d)
 }
 
+func arrivedata(w http.ResponseWriter, r *http.Request) {
+     d := directions.NewDirections(r)
+     d.Directions()
+     data := [][]string{{"Time", "Leave", "Expected", "Delay"}}
+     // for h in drawday(td):
+     //   data.append( stuff from h )
+     b, _ := json.Marshal(data)
+     w.Write(b)
+}
+
 func init() {
+  http.HandleFunc("/arrivedata", arrivedata)
   http.HandleFunc("/arrive", arrive)
   http.HandleFunc("/", hello)
 }
