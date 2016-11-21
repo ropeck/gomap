5806527b6a8b9a2f653af624e8a32bb8e849d60a
Rodney Peck <ropeck@google.com>
2016-11-21 13:27:02 -0800

    arrive added

--- timeplot.go
+++ timeplot.go
@@ -5,14 +5,22 @@ import (
 	"net/http"
 	"html/template"
 )
-var t, err = template.ParseFiles("base.html", "index.html")
 
 func hello(w http.ResponseWriter, r *http.Request) {
+     t, _ := template.ParseFiles("base.html", "index.html")
+     d := directions.NewDirections(r)
+     d.Directions()
+     t.ExecuteTemplate(w, "layout", d)
+}
+
+func arrive(w http.ResponseWriter, r *http.Request) {
+     t, _ := template.ParseFiles("base.html", "arrive.html")
      d := directions.NewDirections(r)
      d.Directions()
      t.ExecuteTemplate(w, "layout", d)
 }
 
 func init() {
+  http.HandleFunc("/arrive", arrive)
   http.HandleFunc("/", hello)
 }
