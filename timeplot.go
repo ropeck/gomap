dd2bec105e4a8e6612ad6c3508876766fc63e05d
Rodney Peck <ropeck@google.com>
2016-11-23 12:11:42 -0800

    applied google formatting

--- timeplot.go
+++ timeplot.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-	"github.com/ropeck/directions"
-	"net/http"
 	"encoding/json"
 	"html/template"
+	"net/http"
 	"time"
+
+	"github.com/ropeck/directions"
 )
 
-func drawday(td time.Time) ([][]string) {
-  return drawday_base(td, false, false)
+func drawday(td time.Time) [][]string {
+	return drawday_base(td, false, false)
 }
 
-func drawday_base(td time.Time, reverse bool, cache bool) ([][]string){
-  data := [][]string{{"time.Time", "Leave", "Expected", "Delay"}}
-  data = append(data, []string{"0","1","2"})
-  return data
+func drawday_base(td time.Time, reverse bool, cache bool) [][]string {
+	data := [][]string{{"Leave", "Expected", "Delay"}}
+	data = append(data, []string{"0", "1", "2"})
+	return data
 }
-
 
 func hello(w http.ResponseWriter, r *http.Request) {
-     t, _ := template.ParseFiles("base.html", "index.html")
-     d := directions.NewDirections(r)
-     d.Directions()
-     t.ExecuteTemplate(w, "layout", d)
+	t, _ := template.ParseFiles("base.html", "index.html")
+	d := directions.NewDirections(r)
+	d.Directions()
+	t.ExecuteTemplate(w, "layout", d)
 }
 
 func arrive(w http.ResponseWriter, r *http.Request) {
-     t, _ := template.ParseFiles("base.html", "arrive.html")
-     d := directions.NewDirections(r)
-     d.Directions()
-     t.ExecuteTemplate(w, "layout", d)
+	t, _ := template.ParseFiles("base.html", "arrive.html")
+	d := directions.NewDirections(r)
+	d.Directions()
+	t.ExecuteTemplate(w, "layout", d)
 }
 
 func arrivedata(w http.ResponseWriter, r *http.Request) {
-     d := directions.NewDirections(r)
-     d.Directions()
-     td := time.Now()
-     data := drawday(td)
-     // for h in drawday(td):
-     //   data.append( stuff from h )
-     b, _ := json.Marshal(data)
-     w.Write(b)
+	d := directions.NewDirections(r)
+	d.Directions()
+	td := time.Now()
+	data := drawday(td)
+	// for h in drawday(td):
+	//   data.append( stuff from h )
+	b, _ := json.Marshal(data)
+	w.Write(b)
 }
 
 func init() {
-  http.HandleFunc("/arrivedata", arrivedata)
-  http.HandleFunc("/arrive", arrive)
-  http.HandleFunc("/", hello)
+	http.HandleFunc("/arrivedata", arrivedata)
+	http.HandleFunc("/arrive", arrive)
+	http.HandleFunc("/", hello)
 }
